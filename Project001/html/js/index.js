39f7aa8d8c4c673737f62f7e68f2428493cb32eb
astm0106 <astm0106@gmail.com>
2021-08-08 18:04:26 +0900

    ボタン追加
    数字入力ボタンを追加

--- Project001/html/js/index.js
+++ Project001/html/js/index.js
@@ -1,11 +1,25 @@
-const dataSample = {
+var dataSample = {
     param1 : "param",
     param2 : 12345
   };
+var int_array = [];
+
+document.addEventListener('DOMContentLoaded',function(){
+    var btns = document.querySelectorAll('#int_btn');
+    for(var i = 0; i < btns.length; i++){
+        btns[i].addEventListener('click',function(){
+            console.log(this.innerHTML);
+            int_array.push(this.innerHTML);
+
+        },false);
+    }
+},false);
 
   // jQuery
   $(function(){
     $('#fin_btn').click(function(event){
+    　　　　alert(int_array);
+
       $.ajax({
         type: "POST",
         url: "tomcat9/searchman/p001_a001",
@@ -15,10 +29,14 @@ const dataSample = {
       // Ajaxリクエストが成功した場合
       .done(function(data){
         $("#text").html(data);
+        int_array = [];
+        window.open('/page003.html', "_blank");
+
       })
       // Ajaxリクエストが失敗した場合
       .fail(function(XMLHttpRequest, textStatus, errorThrown){
-        alert(errorThrown);
+    	  int_array = [];
+    	alert(textStatus);
       });
     });
   });
